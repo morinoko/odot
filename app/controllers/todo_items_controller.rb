4f59ff9c3b089d741686da1fb5ec135973f09c09
Felice Forby <felice@cooksnaps.com>
2014-11-10 16:46:14 +0900

    fix todo_items_controller create action

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -13,7 +13,7 @@ class TodoItemsController < ApplicationController
     @todo_item = @todo_list.todo_items.new(todo_item_params)
     if @todo_item.save
       flash[:success] = "Added todo list item."
-      redirect_to todo_list_todo_items_path
+      redirect_to todo_list_todo_items_path @todo_list
     else
       flash[:error] = "There was a problem adding that todo list item."
       render action: :new
